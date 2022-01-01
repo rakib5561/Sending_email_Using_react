af6f5ecd3109d70e8f178d4ef06e8bc59db9bd04
Md. Rakib <mdrakib5561@gmail.com>
2022-01-01 22:56:10 +0600

    completed

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,33 +1,35 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
+import './Contact.css';
 
 const Contact = () => {
-
+    
     const form = useRef();
+
     const sendEmail = (e) => {
-    e.preventDefault();
+        e.preventDefault();
+    emailjs.sendForm('contact_form', 'contact_n5nscen', form.current, 'user_kb5S5jVfjwnWVrrwzCWx0')
+    .then((result) => {
+        console.log(result.text);
+        alert('Send Your massage.');
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset();
 
-    emailjs.sendForm('gmail', 'template_eytgsgr', form.current, 'user_kb5S5jVfjwnWVrrwzCWx0')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset();
-  };
-
+    };
 
     return (
-        <div>
-            <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="name" />
-      <label>Email</label>
-      <input type="email" name="email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+        <div className='contact-wrapper'>
+            <div className='container'>
+                <h1>Contact Form</h1>
+                <form className='conatct-form' ref={form} onSubmit={sendEmail}>
+                    <input type="text" placeholder='Your Name' name='name' />
+                    <input type="email" placeholder='Your Email' name='email' />
+                    <textarea name="massage" id="" cols="30" rows="10" placeholder='enter your massage ...'></textarea>
+                    <input className='contact-btn' type="submit" value='Submit'/>
+                </form>    
+            </div>
         </div>
     );
 };
